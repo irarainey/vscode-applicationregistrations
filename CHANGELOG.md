9a4620e9c8a307d1cd03f8cd64251d0d98a30b1e
Ira Rainey <irarainey@microsoft.com>
2023-02-07 09:26:53 +0000

    Added sign in and sign out command handlers.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,8 @@
 ## 0.7.0
 
 - Added commands to sign in and sign out of Azure CLI from the command palette.
-- Refactored tree rendering trigger to facilitate better unit testing.
 - Added command filters to ensure those that aren't relevant are't shown in the palette.
+- Refactored tree rendering trigger to facilitate better unit testing.
 - Fixed an issue where the wrong error message was passed.
 
 ## 0.6.0
--- src/utils/cli-authentication.ts
+++ src/utils/cli-authentication.ts
@@ -1,35 +1,84 @@
 import { window } from "vscode";
 import { execShellCmd } from "../utils/exec-shell-cmd";
+import { setStatusBarMessage } from "../utils/status-bar";
+import { errorHandler } from "../error-handler";
+import { AppRegTreeDataProvider } from "../data/app-reg-tree-data-provider";
 
 // Invokes the Azure CLI sign-in command to authenticate the user.
-export const authenticate = async (): Promise<boolean | undefined> => {
-    // Prompt the user for the tenant name or Id.
-    const tenant = await window.showInputBox({
-        placeHolder: "Tenant Name or ID",
-        prompt: "Enter the tenant name or ID, or leave blank for the default tenant",
-        title: "Azure CLI Sign-In Tenant",
-        ignoreFocusOut: true
-    });
+export const signInToCli = async (treeDataProvider: AppRegTreeDataProvider) => {
+	await treeDataProvider.render(undefined, "AUTHENTICATING");
+	const status = await authenticate();
 
-    // If the tenant is undefined then we don't want to do anything because they pressed cancel.
-    if (tenant === undefined) {
-        return undefined;
-    }
+	// The user pressed cancel.
+	if (status === undefined) {
+		// Set the tree view to initialising.
+		await treeDataProvider.render(undefined, "INITIALISING");
 
-    // Build the command to invoke the Azure CLI sign-in command.
-    let command = "az login";
-    if (tenant.length > 0) {
-        command += ` --tenant ${tenant}`;
-    }
+		// Initialise the graph repository.
+		const status = await treeDataProvider.graphRepository.initialise();
 
-    // Execute the command.
-    const status = await execShellCmd(command)
-        .then(() => {
-            return true;
-        })
-        .catch(() => {
-            return false;
-        });
+		// Were we able to get an access token?
+		if (status !== true) {
+			// If not then the user needs to authenticate.
+			await treeDataProvider.render(undefined, "SIGN-IN");
+		} else {
+			// If so then the user is authenticated so load the tree view.
+			await treeDataProvider.render(setStatusBarMessage("Loading Application Registrations..."));
+		}
 
-    return status;
+		return;
+	}
+
+	// If there was a problem authenticating then show the sign in option.
+	if (status !== true) {
+		await treeDataProvider.render(undefined, "SIGN-IN");
+		return;
+	}
+
+	// If the user was successfully authenticated then load the list
+	await treeDataProvider.render(setStatusBarMessage("Loading Application Registrations..."), "AUTHENTICATED");
+};
+
+// Invokes the Azure CLI sign-out command to sign the user out.
+export const signOutFromCli = async (treeDataProvider: AppRegTreeDataProvider) => {
+	const status = await execShellCmd("az logout")
+		.then(async () => {
+			await treeDataProvider.render(undefined, "SIGN-IN");
+		})
+		.catch(async (error) => {
+			await errorHandler(error);
+		});
+};
+
+// Invokes the Azure CLI sign-in command to authenticate the user.
+const authenticate = async (): Promise<boolean | undefined> => {
+	// Prompt the user for the tenant name or Id.
+	const tenant = await window.showInputBox({
+		placeHolder: "Tenant Name or ID",
+		prompt: "Enter the tenant name or ID, or leave blank for the default tenant",
+		title: "Azure CLI Sign-In Tenant",
+		ignoreFocusOut: true
+	});
+
+	// If the tenant is undefined then we don't want to do anything because they pressed cancel.
+	if (tenant === undefined) {
+		return undefined;
+	}
+
+	// Build the command to invoke the Azure CLI sign-in command.
+	let command = "az login";
+	if (tenant.length > 0) {
+		command += ` --tenant ${tenant}`;
+	}
+
+	// Execute the command.
+	const status = await execShellCmd(command)
+		.then(() => {
+			return true;
+		})
+		.catch(() => {
+			return false;
+		});
+
+	return status;
 };
